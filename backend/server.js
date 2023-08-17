3cff3624e4838332f10a42628901760ef163c397
Charles Louis <charleslouis1234@gmail.com>
2023-08-17 09:33:16 +0530

    upto profile updation without image

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,35 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
 dotenv.config();
-import cookieParser from 'cookie-parser';
-import { notFound,errorHandler } from './middleware/errorMiddleware.js';
-import mongoDBConnect from './config/db.js';
+import cookieParser from "cookie-parser";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import mongoDBConnect from "./config/db.js";
 
-import userRoutes from './routes/userRoutes.js'
+import userRoutes from "./routes/userRoutes.js";
 const PORT = process.env.PORT || 5000;
 mongoDBConnect();
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true})); 
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-app.use('/api/users',userRoutes)
+app.use("/api/users", userRoutes);
 
-app.get('/',(req,res)=> res.send('Server is ready'))
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => res.send("Server is ready"));
+}
 
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(PORT,()=>console.log(`server started on port ${PORT}`));
-
- 
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -11,8 +11,6 @@ import { setCredentials } from "../slices/authSlice";
 const ProfileScreen = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -26,7 +24,6 @@ const ProfileScreen = () => {
     setEmail(userInfo.email);
   }, [userInfo.setName, userInfo.setEmail]);
 
-
   const submitHandler = async (e) => {
     e.preventDefault();
     if (password !== confirmPassword) {
@@ -34,16 +31,15 @@ const ProfileScreen = () => {
     } else {
       try {
         const res = await updateProfile({
-          _id: userInfo.data._id,
+          _id: userInfo._id,
           name,
           email,
-          password,
         }).unwrap();
         dispatch(setCredentials({ ...res }));
-        toast.success('Profile Updated')
+        toast.success("Profile Updated");
       } catch (err) {
-      toast.error(err?.data?.message || err?.error);
-    }
+        toast.error(err?.data?.message || err?.error);
+      }
     }
   };
 
@@ -68,26 +64,6 @@ const ProfileScreen = () => {
             placeholder="Enter email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-
-        <Form.Group className="my-2" controlId="password">
-          <Form.Label>Password </Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-
-        <Form.Group className="my-2" controlId="confirmPassword">
-          <Form.Label>Confirm Password </Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter confirmPassword"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
